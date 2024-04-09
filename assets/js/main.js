393383e7f9356d8ab0a6ac00c4cb170f837bee58
senorfetty <billykimbett@gmail.com>
2024-04-09 11:23:03 +0300

    +

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,29 +42,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
   /**
    * Navbar links active state on scroll
-   */
-  let navbarlinks = document.querySelectorAll('#navbar a');
+  /**
+ * Navbar links active state on scroll
+ */
+let navbarlinks = document.querySelectorAll('#navbar a');
 
-  function navbarlinksActive() {
-    navbarlinks.forEach(navbarlink => {
+function navbarlinksActive() {
+  navbarlinks.forEach(navbarlink => {
 
-      if (!navbarlink.hash) return;
+    if (!navbarlink.hash) return;
 
-      let section = document.querySelector(navbarlink.hash);
-      if (!section) return;
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
 
-      let position = window.scrollY + 200;
+    let position = window.scrollY + 200;
+    let headerHeight = selectHeader.offsetHeight; // Height of the sticky header
 
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active');
-      } else {
-        navbarlink.classList.remove('active');
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive);
-  document.addEventListener('scroll', navbarlinksActive);
+    if (
+      position >= section.offsetTop - headerHeight &&
+      position <= (section.offsetTop + section.offsetHeight - headerHeight)
+    ) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
+    }
+  });
+}
 
+window.addEventListener('load', navbarlinksActive);
+window.addEventListener('scroll', function() {
+  navbarlinksActive();
+});
   /**
    * Mobile nav toggle
    */
